542bb787672d8e032d3172b645c42ae74967ec68
praveenkumar-s <praveenkumar.s@realimage.com>
2018-09-25 17:58:45 +0530

    Add new feature importer

---
+++ aha_zen_master_feature_importer.py
@@ -0,0 +1,27 @@
+# Get list if epics from Zenhub
+# Get details about the epic
+# Create master features on Aha!. Decide upon the features that needs to be added on to it
+# Find a way to link user stories to respective epics
+# Use database for communication : preferebly firebase
+
+#get list of epics from Zen and return
+def getListOfEpicsZen():
+    pass
+
+def getEpicDataGit():
+    pass
+
+def getMasterFeatureAha():
+    pass
+
+def insertMasterFeatureAha():
+    pass
+
+
+def updateMasterFeatureAha():
+    pass
+
+
+def updateFeaturesAha():
+    pass
+
