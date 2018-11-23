dbc13afd81435471d10b7eaf77a74bb177eea17d
praveenkumar-s <praveenkumar.s@realimage.com>
2018-11-23 17:46:22 +0530

    Fixed bytes problem

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def upload_to_storage(data):
     rs=requests.post('https://funteam.herokuapp.com/insertresults', data= str(data))
     if(rs.status_code==200):
-        return 'https://funteam.herokuapp.com/getautomationresults?key='+rs.content
+        return 'https://funteam.herokuapp.com/getautomationresults?key='+str(rs.content)
     else:
         return ''
 
