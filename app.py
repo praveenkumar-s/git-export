0ce4d54182d27939f936cee5c759401c79e6d08c
praveenkumar-s <praveenkumar.s@realimage.com>
2019-03-19 11:34:35 +0530

    Mirroing or env variables

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import slack_sender
-import requests
 from datetime import datetime
 import sys
 from objectifier import Objectifier
 import json
 import os
-import releases
+import requests
 
 def upload_to_storage(data):
     rs=requests.post('https://funteam.herokuapp.com/insertresults', data= str(data)+ '     \n This Ran @ : '+str(datetime.now()))
@@ -19,7 +18,10 @@ def upload_to_storage(data):
 def main():
     config_selector= sys.argv[1]
     config= json.loads(os.environ.get(config_selector))
+    os.environ['config']=os.environ.get(config_selector)
+    print(os.environ['config'])
     config=Objectifier(config)
+    import releases
     import aha_zen_adapter
     import aha_zen_master_feature_importer
     releases.main()
